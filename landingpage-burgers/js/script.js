8713e281f33cba12ebb8df098ef430dcc480e169
Tam <tambuildsthings@gmail.com>
2023-05-21 20:08:16 +0200

    Tweaks and fiddles

--- landingpage-burgers/js/script.js
+++ landingpage-burgers/js/script.js
@@ -1,8 +1,16 @@
 const btn = document.getElementById('menu-btn')
 const menu = document.getElementById('menu')
+const reviewsBtn = document.getElementById('load-reviews-btn')
+const reviewsCol2 = document.getElementById('reviews-col-2')
+const reviewsCol3 = document.getElementById('reviews-col-3')
+let reviewColsShowing = 1
+
 
 // listen for click on menu button
 btn.addEventListener('click', navToggle)
+
+// listen for click anywhere in the menu
+menu.addEventListener('click', navToggle)
 
 function navToggle(){
     btn.classList.toggle('open')
@@ -10,4 +18,20 @@ function navToggle(){
     menu.classList.toggle('hidden')
 }
 
-// TODO - Get the load-reviews-btn to reveal more reviews on mobile
+// listen for click on Load more reviews button
+reviewsBtn.addEventListener('click', reviewShowMore)
+
+// function to show the additional columns
+function reviewShowMore(){
+    console.log(reviewColsShowing)
+    if (reviewColsShowing === 1) {
+        reviewsCol2.classList.remove('hidden')
+        reviewsCol2.classList.add('inline-grid')
+        reviewColsShowing++
+    } else if (reviewColsShowing === 2) {
+        reviewsCol3.classList.remove('hidden')
+        reviewsCol3.classList.add('inline-grid')
+        reviewsBtn.classList.add('hidden')
+    }
+    
+}
